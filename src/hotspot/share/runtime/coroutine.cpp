e7ae53af46d0c509f32d6991ce9cc222c9ad1245
yunyao.zxl <yunyao.zxl@alibaba-inc.com>
2019-11-28 17:46:28 +0800

    [Wisp] Remove JKU stack trace fp logic
    Summary: We'd remove the `fp == NULL` check: fp is useless in frame->sender() logic when frame is an entry frame (JavaCalls::call()) / compiled frame. Also, after using -XX:-PreserveFramePointer, the rbp register on X86 and the rfp register (alias x29 register) on AArch64 will be used as a general register which will be modified by the compiler.
    
    Test Plan: all wisp tests, also run several days specjbb on ARM to test whether there's GC problem after this patch.
    
    Reviewers: shiyue.xw,lei.yul,sanhong.lsh,chuansheng.lcs,zhuoren.wz
    
    Issue: https://aone.alibaba-inc.com/task/23721353
    
    CR: https://code.aone.alibaba-inc.com/xcode/jdk11/codereview/2153485

--- src/hotspot/share/runtime/coroutine.cpp
+++ src/hotspot/share/runtime/coroutine.cpp
@@ -471,17 +471,24 @@ void CoroutineStack::frames_do(FrameClosure* fc) {
   DEBUG_CORO_ONLY(tty->print_cr("frames_do stack %08x", _stack_base));
 
   intptr_t* fp = ((intptr_t**)_last_sp)[0];
-  if (fp != NULL) {
-    address pc = ((address*)_last_sp)[1];
-    intptr_t* sp = ((intptr_t*)_last_sp) + 2;
+  address pc = ((address*)_last_sp)[1];
+  intptr_t* sp = ((intptr_t*)_last_sp) + 2;
 
-    frame fr(sp, fp, pc);
-    StackFrameStream fst(_thread, fr);
-    fst.register_map()->set_location(rbp->as_VMReg(), (address)_last_sp);
-    fst.register_map()->set_include_argument_oops(false);
-    for(; !fst.is_done(); fst.next()) {
+  frame fr(sp, fp, pc);
+
+#ifdef ASSERT
+  // if fp == NULL, it must be that frame pointer register is used as a general register
+  // in compiled code and the frame pointer register is written to 0 when the code's running.
+  if (fp == NULL) {
+    assert(fr.is_first_frame() || (!PreserveFramePointer && !fr.is_interpreted_frame() && CodeCache::find_blob(pc)), "FP is NULL only if sender frame is a JavaCalls::call() frame, or a compiler frame && -XX:-PreserveFramePointer");
+  }
+#endif
+
+  StackFrameStream fst(_thread, fr);
+  fst.register_map()->set_location(rbp->as_VMReg(), (address)_last_sp);
+  fst.register_map()->set_include_argument_oops(false);
+  for(; !fst.is_done(); fst.next()) {
       fc->frames_do(fst.current(), fst.register_map());
-    }
   }
 }
 
@@ -489,15 +496,20 @@ frame CoroutineStack::last_frame(Coroutine* coro, RegisterMap& map) const {
   DEBUG_CORO_ONLY(tty->print_cr("last_frame CoroutineStack"));
 
   intptr_t* fp = ((intptr_t**)_last_sp)[0];
-  assert(fp != NULL, "coroutine with NULL fp");
-
   address pc = ((address*)_last_sp)[1];
   intptr_t* sp = ((intptr_t*)_last_sp) + 2;
 
   map.set_location(rbp->as_VMReg(), (address)_last_sp);
   map.set_include_argument_oops(false);
 
-  return frame(sp, fp, pc);
+  frame f(sp, fp, pc);
+#ifdef ASSERT
+  if (fp == NULL) {
+    assert(f.is_first_frame() || (!PreserveFramePointer && !f.is_interpreted_frame() && CodeCache::find_blob(pc)), "FP is NULL only if sender frame is a JavaCalls::call() frame, or a compiler frame && -XX:-PreserveFramePointer");
+  }
+#endif
+
+  return f;
 }
 
 oop Coroutine::print_stack_header_on(outputStream* st) {
---
+++ test/hotspot/jtreg/runtime/coroutine/TestPreserveFramePointer.java
@@ -0,0 +1,55 @@
+/*
+ * @test
+ * @summary PreserveFramePointer for coroutine stack backtrace test
+ * @run main/othervm -XX:-UseBiasedLocking -XX:+EnableCoroutine -XX:+UseWispMonitor -Dcom.alibaba.wisp.transparentWispSwitch=true -Dcom.alibaba.wisp.version=2 -Xcomp -XX:TieredStopAtLevel=1 -XX:+PreserveFramePointer TestPreserveFramePointer
+ * @run main/othervm -XX:-UseBiasedLocking -XX:+EnableCoroutine -XX:+UseWispMonitor -Dcom.alibaba.wisp.transparentWispSwitch=true -Dcom.alibaba.wisp.version=2 -Xcomp -XX:TieredStopAtLevel=1 -XX:-PreserveFramePointer TestPreserveFramePointer
+ * @run main/othervm -XX:-UseBiasedLocking -XX:+EnableCoroutine -XX:+UseWispMonitor -Dcom.alibaba.wisp.transparentWispSwitch=true -Dcom.alibaba.wisp.version=2 -Xcomp -XX:-TieredCompilation -XX:+PreserveFramePointer TestPreserveFramePointer
+ * @run main/othervm -XX:-UseBiasedLocking -XX:+EnableCoroutine -XX:+UseWispMonitor -Dcom.alibaba.wisp.transparentWispSwitch=true -Dcom.alibaba.wisp.version=2 -Xcomp -XX:-TieredCompilation -XX:-PreserveFramePointer TestPreserveFramePointer
+ */
+
+
+import com.alibaba.wisp.engine.WispEngine;
+
+import java.util.concurrent.CountDownLatch;
+
+public class TestPreserveFramePointer {
+
+    private static Object lock = new Object();
+
+    private static final int COROUTINE_NUM = 100;
+
+    private static CountDownLatch cdl = new CountDownLatch(1);
+    private static CountDownLatch mainLock = new CountDownLatch(COROUTINE_NUM);
+
+    public static void main(String[] args) throws InterruptedException {
+
+        new Thread(() -> {                    // the lock holder thread to make lock contend
+            synchronized (lock) {
+                cdl.countDown();              // when we hold the lock, others can go on
+                try {
+                    Thread.sleep(100);  // hold the lock for 100ms so others contends
+                    System.gc();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            while (true) {}
+        }).start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                WispEngine.dispatch(() -> {
+                    try {
+                        cdl.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    mainLock.countDown();
+                });
+            }
+        }).start();
+
+        mainLock.await();
+    }
+
+}
