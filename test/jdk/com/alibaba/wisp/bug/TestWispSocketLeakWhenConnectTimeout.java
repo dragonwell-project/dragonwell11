4834d0f180c63f3a08024d3d9735d9046e012a16
yunyao.zxl <yunyao.zxl@alibaba-inc.com>
2020-05-15 16:49:35 +0800

    [Wisp] Revert back TestWispSocketLeakWhenConnectTimeout.java
    Summary: This test needs a timeout.
    
    Test Plan: The test itself
    
    Reviewers: shiyue.xw, lei.yul
    
    Issue: https://aone.alibaba-inc.com/task/27728272
    
    CR: https://code.aone.alibaba-inc.com/xcode/jdk11/codereview/2982335

--- test/jdk/com/alibaba/wisp/bug/TestWispSocketLeakWhenConnectTimeout.java
+++ test/jdk/com/alibaba/wisp/bug/TestWispSocketLeakWhenConnectTimeout.java
@@ -3,39 +3,21 @@
  * @library /lib/testlibrary
  * @summary test the fix to fd leakage when socket connect timeout
  * @run main/othervm -XX:+EnableCoroutine -Dcom.alibaba.wisp.transparentWispSwitch=true  TestWispSocketLeakWhenConnectTimeout
-*/
+ */
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
-import java.util.Properties;
 
 import static jdk.testlibrary.Asserts.assertTrue;
 
 public class TestWispSocketLeakWhenConnectTimeout {
-
-    static Properties p;
-    static String socketAddr;
-    static {
-        p = java.security.AccessController.doPrivileged(
-                new java.security.PrivilegedAction<Properties>() {
-                    public Properties run() {
-                        return System.getProperties();
-                    }
-                }
-        );
-        socketAddr = (String)p.get("test.wisp.socketAddress");
-        if (socketAddr == null) {
-            socketAddr = "www.example.com";
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         Socket so = new Socket();
         boolean timeout = false;
         try {
-            so.connect(new InetSocketAddress(socketAddr, 80), 5);
+            so.connect(new InetSocketAddress("www.example.com", 80), 5);
         } catch (SocketTimeoutException e) {
             assertTrue(so.isClosed());
             timeout = true;
