6f5af9e6ecd305028f60a22bebc8156523a29076
Hao Tang <albert.th@alibaba-inc.com>
2021-03-19 10:34:46 +0800

    [GC] 8217857: ZGC: Move SvcGCMarker to ZServiceabilityTracer
    Summary: Backport JDK-8217857 for ZGC.
      https://github.com/openjdk/jdk/commit/62e8d6f43b3
      Conflicts:
        the include statements of the two files
    
    Reviewed-by: mmyxym, linade
    
    Test Plan: test/hotspot/jtreg/gc
    
    Issue: https://github.com/alibaba/dragonwell11/issues/76

--- src/hotspot/share/gc/z/zDriver.cpp
+++ src/hotspot/share/gc/z/zDriver.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -98,9 +98,6 @@ public:
     assert(SafepointSynchronize::is_at_safepoint(), "Should be at safepoint");
 
     ZStatSample(ZSamplerJavaThreads, Threads::number_of_threads());
-
-    // JVMTI support
-    SvcGCMarker sgcm(SvcGCMarker::CONCURRENT);
 
     // Setup GC id
     GCIdMark gcid(_gc_id);
