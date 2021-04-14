c9891e162da4c2618070eaa5439f127f00da20db
Hao Tang <albert.th@alibaba-inc.com>
2021-04-14 16:31:33 +0800

    [GC] 8222480: Implementation: JEP 351: ZGC: Uncommit Unused Memory
    Summary: Backport JDK-8222480 for ZGC.
      https://github.com/openjdk/jdk/commit/44e7959bb877b17b
      Conflicts:
        src/hotspot/share/gc/z/zPage.inline.hpp
        src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/gc/z/ZPageAllocator.java
      Modification:
        src/hotspot/share/gc/z/zHeap.cpp
        src/hotspot/share/gc/z/zUncommitter.cpp (MonitorLockerEx)
    
    Reviewed-by: mmyxym, linade
    
    Test Plan: test/hotspot/jtreg/gc
    
    Issue: https://github.com/alibaba/dragonwell11/issues/85

--- src/hotspot/share/gc/z/zCollectedHeap.hpp
+++ src/hotspot/share/gc/z/zCollectedHeap.hpp
@@ -30,10 +30,11 @@
 #include "gc/z/zCollectorPolicy.hpp"
 #include "gc/z/zDirector.hpp"
 #include "gc/z/zDriver.hpp"
-#include "gc/z/zInitialize.hpp"
 #include "gc/z/zHeap.hpp"
+#include "gc/z/zInitialize.hpp"
 #include "gc/z/zRuntimeWorkers.hpp"
 #include "gc/z/zStat.hpp"
+#include "gc/z/zUncommitter.hpp"
 
 class ZCollectedHeap : public CollectedHeap {
   friend class VMStructs;
@@ -46,6 +47,7 @@ private:
   ZHeap             _heap;
   ZDirector*        _director;
   ZDriver*          _driver;
+  ZUncommitter*     _uncommitter;
   ZStat*            _stat;
   ZRuntimeWorkers   _runtime_workers;
 
--- src/hotspot/share/gc/z/zHeap.hpp
+++ src/hotspot/share/gc/z/zHeap.hpp
@@ -64,6 +64,7 @@ private:
   ZServiceability     _serviceability;
 
   size_t heap_min_size() const;
+  size_t heap_initial_size() const;
   size_t heap_max_size() const;
   size_t heap_max_reserve_size() const;
 
@@ -127,6 +128,9 @@ public:
   ZPage* alloc_page(uint8_t type, size_t size, ZAllocationFlags flags);
   void undo_alloc_page(ZPage* page);
   void free_page(ZPage* page, bool reclaimed);
+
+  // Uncommit memory
+  uint64_t uncommit(uint64_t delay);
 
   // Object allocation
   uintptr_t alloc_tlab(size_t size);
