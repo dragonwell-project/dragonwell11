d22cd73d107cf016a7f396b4e6d90109bc818c3a
Hao Tang <albert.th@alibaba-inc.com>
2021-04-14 16:31:33 +0800

    [GC] 8222480: Implementation: JEP 351: ZGC: Uncommit Unused Memory
    Summary: Backport JDK-8222480 for ZGC.
      https://github.com/openjdk/jdk/commit/44e7959bb877b17b
      Conflicts:
        src/hotspot/share/gc/z/zPage.inline.hpp
        src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/gc/z/ZPageAllocator.java
      Modification:
        src/hotspot/share/gc/z/zHeap.cpp
        src/hotspot/share/gc/z/zUncommitter.cpp (MonitorLockerEx)
    
    Reviewed-by: mmyxym, linade
    
    Test Plan: test/hotspot/jtreg/gc
    
    Issue: https://github.com/alibaba/dragonwell11/issues/85

--- src/hotspot/share/gc/z/zCollectedHeap.cpp
+++ src/hotspot/share/gc/z/zCollectedHeap.cpp
@@ -47,6 +47,7 @@ ZCollectedHeap::ZCollectedHeap(ZCollectorPolicy* policy) :
     _heap(),
     _director(new ZDirector()),
     _driver(new ZDriver()),
+    _uncommitter(new ZUncommitter()),
     _stat(new ZStat()),
     _runtime_workers() {}
 
@@ -76,6 +77,7 @@ void ZCollectedHeap::initialize_serviceability() {
 void ZCollectedHeap::stop() {
   _director->stop();
   _driver->stop();
+  _uncommitter->stop();
   _stat->stop();
 }
 
@@ -286,6 +288,7 @@ jlong ZCollectedHeap::millis_since_last_gc() {
 void ZCollectedHeap::gc_threads_do(ThreadClosure* tc) const {
   tc->do_thread(_director);
   tc->do_thread(_driver);
+  tc->do_thread(_uncommitter);
   tc->do_thread(_stat);
   _heap.worker_threads_do(tc);
   _runtime_workers.threads_do(tc);
@@ -336,6 +339,8 @@ void ZCollectedHeap::print_gc_threads_on(outputStream* st) const {
   _director->print_on(st);
   st->cr();
   _driver->print_on(st);
+  st->cr();
+  _uncommitter->print_on(st);
   st->cr();
   _stat->print_on(st);
   st->cr();
