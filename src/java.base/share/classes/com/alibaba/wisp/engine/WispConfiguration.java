5f8df2d26aeb57212f23dc155aff75973bb1a7c4
lei.yul <lei.yul@alibaba-inc.com>
2021-08-19 21:59:31 +0800

    [Wisp] Thread as Wisp whitelist
    Summary: Provides a way to run thread as wisp using
    configuration
    
    Test Plan: TestThreadAsWispWhiteList
    
    Reviewed-by: joeylee.lz, sanhong.lsh
    
    Issue: https://teambition.alibaba-inc.com/task/611e61276d16cd58aab956d0
    
    CR: https://code.aone.alibaba-inc.com/xcode/jdk11/codereview/6228589

--- src/java.base/share/classes/com/alibaba/wisp/engine/WispConfiguration.java
+++ src/java.base/share/classes/com/alibaba/wisp/engine/WispConfiguration.java
@@ -57,6 +57,7 @@ class WispConfiguration {
     static final int WISP_CONTROL_GROUP_CFS_PERIOD;
 
     private static List<String> THREAD_AS_WISP_BLACKLIST;
+    private static List<String> THREAD_AS_WISP_WHITELIST;
 
     static {
         Properties p = java.security.AccessController.doPrivileged(
@@ -133,10 +134,6 @@ class WispConfiguration {
                 ENABLE_THREAD_AS_WISP, "-Dcom.alibaba.wisp.enableThreadAsWisp=true");
         checkDependency(CARRIER_AS_POLLER, "-Dcom.alibaba.wisp.useCarrierAsPoller=true",
                 ALL_THREAD_AS_WISP, "-Dcom.alibaba.wisp.allThreadAsWisp=true");
-        if (ENABLE_THREAD_AS_WISP && !ALL_THREAD_AS_WISP) {
-            throw new IllegalArgumentException("shift thread model by stack configuration is no longer supported," +
-                    " use -XX:+UseWisp2 instead");
-        }
     }
 
     private static void checkDependency(boolean cond, String condStr, boolean preRequire, String preRequireStr) {
@@ -205,7 +202,7 @@ class WispConfiguration {
             }
         }
         THREAD_AS_WISP_BLACKLIST = parseListParameter(p, confProp, "com.alibaba.wisp.threadAsWisp.black");
-
+        THREAD_AS_WISP_WHITELIST = parseListParameter(p, confProp, "com.alibaba.wisp.threadAsWisp.white");
     }
 
     private static final int UNLOADED = 0, LOADING = 1, LOADED = 2;
@@ -229,5 +226,11 @@ class WispConfiguration {
         ensureBizConfigLoaded();
         assert THREAD_AS_WISP_BLACKLIST != null;
         return THREAD_AS_WISP_BLACKLIST;
+    }
+
+    static List<String> getThreadAsWispWhitelist() {
+        ensureBizConfigLoaded();
+        assert THREAD_AS_WISP_WHITELIST != null;
+        return THREAD_AS_WISP_WHITELIST;
     }
 }
