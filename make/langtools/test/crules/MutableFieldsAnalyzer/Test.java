a291c37890eb203eead16fd6223696b632cf038f
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-25 16:45:07 +0800

    Delete make directory

--- make/langtools/test/crules/MutableFieldsAnalyzer/Test.java
+++
@@ -1,9 +0,0 @@
-/**@test /nodynamiccopyright/
- * @compile/fail/ref=Test.out -Xplugin:coding_rules -XDrawDiagnostics Test.java
- */
-
-package com.sun.tools.javac;
-
-public class Test {
-    public static String mutable;
-}
