6597f1a48319676a27e86c62367e5c85426d89c3
Jin,Zhonghui <zhonghui.jin@intel.com>
2023-01-05 17:38:00 +0800

    [Backport] Subset of 8223320 for VectorAPI: 8223347
    Summary: Subset of 8223320: [AOT] jck test api/javax_script/ScriptEngine/PutGet.html fails when test classes are AOTed. For VectorAPI: 8223347.
    
    Test Plan: ci jtreg
    
    Reviewed-by: JoshuaZhuwj
    
    Issue: https://github.com/alibaba/dragonwell11/issues/361

--- src/hotspot/share/runtime/deoptimization.hpp
+++ src/hotspot/share/runtime/deoptimization.hpp
@@ -159,7 +159,7 @@ class Deoptimization : AllStatic {
 JVMCI_ONLY(public:)
 
   // Support for restoring non-escaping objects
-  static bool realloc_objects(JavaThread* thread, frame* fr, GrowableArray<ScopeValue*>* objects, TRAPS);
+  static bool realloc_objects(JavaThread* thread, frame* fr, RegisterMap* reg_map, GrowableArray<ScopeValue*>* objects, TRAPS);
   static void reassign_type_array_elements(frame* fr, RegisterMap* reg_map, ObjectValue* sv, typeArrayOop obj, BasicType type);
   static void reassign_object_array_elements(frame* fr, RegisterMap* reg_map, ObjectValue* sv, objArrayOop obj);
   static void reassign_fields(frame* fr, RegisterMap* reg_map, GrowableArray<ScopeValue*>* objects, bool realloc_failures, bool skip_internal);
