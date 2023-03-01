7638fcd7ff6aa33a54dc38dde4900a63ec9933af
Jin,Zhonghui <zhonghui.jin@intel.com>
2023-03-01 15:21:52 +0800

    [Backport] 8259775: [Vector API] Incorrect code-gen for VectorReinterpret operation
    Summary: [Backport] 8259775: [Vector API] Incorrect code-gen for VectorReinterpret operation
    
    Test Plan: ci jtreg
    
    Reviewed-by: JoshuaZhuwj
    
    Issue: https://github.com/alibaba/dragonwell11/issues/462

--- src/hotspot/cpu/x86/macroAssembler_x86.cpp
+++ src/hotspot/cpu/x86/macroAssembler_x86.cpp
@@ -3568,7 +3568,6 @@ void MacroAssembler::movdqu(XMMRegister dst, Address src) {
 
 void MacroAssembler::movdqu(XMMRegister dst, XMMRegister src) {
     assert(((dst->encoding() < 16  && src->encoding() < 16) || VM_Version::supports_avx512vl()),"XMM register should be 0-15");
-    if (dst->encoding() == src->encoding()) return;
     Assembler::movdqu(dst, src);
 }
 
@@ -3593,7 +3592,6 @@ void MacroAssembler::vmovdqu(XMMRegister dst, Address src) {
 
 void MacroAssembler::vmovdqu(XMMRegister dst, XMMRegister src) {
     assert(((dst->encoding() < 16  && src->encoding() < 16) || VM_Version::supports_avx512vl()),"XMM register should be 0-15");
-    if (dst->encoding() == src->encoding()) return;
     Assembler::vmovdqu(dst, src);
 }
 
