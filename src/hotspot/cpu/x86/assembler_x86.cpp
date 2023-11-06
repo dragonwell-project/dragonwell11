970d050fdff96a54aa64cdac238b8af2d4f5d6e1
Jin,Zhonghui <zhonghui.jin@intel.com>
2023-11-07 01:18:09 +0800

    [Backport] 8256973: Intrinsic creation for VectorMask query (lastTrue,firstTrue,trueCount) APIs
    Summary: [Backport] 8256973: Intrinsic creation for VectorMask query (lastTrue,firstTrue,trueCount) APIs
    
    Test Plan: ci jtreg
    
    Reviewed-by: JoshuaZhuwj
    
    Issue: https://github.com/dragonwell-project/dragonwell11/issues/615

--- src/hotspot/cpu/x86/assembler_x86.cpp
+++ src/hotspot/cpu/x86/assembler_x86.cpp
@@ -9493,6 +9493,14 @@ void Assembler::shlxq(Register dst, Register src1, Register src2) {
   emit_int8((unsigned char)(0xC0 | encode));
 }
 
+void Assembler::evpmovb2m(KRegister dst, XMMRegister src, int vector_len) {
+  assert(VM_Version::supports_avx512vlbw(), "");
+  InstructionAttr attributes(vector_len, /* vex_w */ false, /* legacy_mode */ false, /* no_mask_reg */ true, /* uses_vl */ true);
+  attributes.set_is_evex_instruction();
+  int encode = vex_prefix_and_encode(dst->encoding(), 0, src->encoding(), VEX_SIMD_F3, VEX_OPCODE_0F_38, &attributes);
+  emit_int16(0x29, (0xC0 | encode));
+}
+
 #ifndef _LP64
 
 void Assembler::incl(Register dst) {
