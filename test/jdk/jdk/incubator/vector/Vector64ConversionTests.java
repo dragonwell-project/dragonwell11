40fa4da841ab70de6ef9d64dbf947d4842b403ea
Jin,Zhonghui <zhonghui.jin@intel.com>
2023-11-28 18:49:13 +0800

    [Backport] 8268293: VectorAPI cast operation on mask and shuffle is broken
    Summary: 8268293: VectorAPI cast operation on mask and shuffle is broken
    
    Test Plan: ci jtreg
    
    Reviewed-by: JoshuaZhuwj
    
    Issue: https://github.com/dragonwell-project/dragonwell11/issues/585

--- test/jdk/jdk/incubator/vector/Vector64ConversionTests.java
+++ test/jdk/jdk/incubator/vector/Vector64ConversionTests.java
@@ -22,12 +22,15 @@
  */
 
 import jdk.incubator.vector.VectorShape;
+import jdk.incubator.vector.VectorMask;
+import jdk.incubator.vector.VectorShuffle;
 import jdk.incubator.vector.VectorSpecies;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.Assert;
 
 import java.util.function.IntFunction;
-
+import java.util.List;
 
 /**
  * @test
@@ -47,6 +50,17 @@ import java.util.function.IntFunction;
  *      Vector64ConversionTests
  */
 
+/*
+ * @test VectorConversionPPC64
+ * @bug 8256479
+ * @requires os.arch == "ppc64" | os.arch == "ppc64le"
+ * @summary VectorConversion on PPC64 without Vector Register usage
+ * @modules jdk.incubator.vector
+ * @modules java.base/jdk.internal.vm.annotation
+ * @run testng/othervm/timeout=1800  -XX:-SuperwordUseVSX -XX:-TieredCompilation --add-opens jdk.incubator.vector/jdk.incubator.vector=ALL-UNNAMED
+ * Vector64ConversionTests
+ */
+
 @Test
 public class Vector64ConversionTests extends AbstractVectorConversionTest {
 
@@ -61,6 +75,16 @@ public class Vector64ConversionTests extends AbstractVectorConversionTest {
     @DataProvider
     public Object[][] fixedShapeXShape() {
         return fixedShapeXShapeSpeciesArgs(SHAPE);
+    }
+
+    @DataProvider
+    public Object[][] fixedShapeXSegmentedLegalCastSpecies() {
+        return fixedShapeXSegmentedCastSpeciesArgs(SHAPE, true);
+    }
+
+    @DataProvider
+    public Object[][] fixedShapeXSegmentedIllegalCastSpecies() {
+        return fixedShapeXSegmentedCastSpeciesArgs(SHAPE, false);
     }
 
     @Test(dataProvider = "fixedShapeXfixedShape")
@@ -85,5 +109,25 @@ public class Vector64ConversionTests extends AbstractVectorConversionTest {
     static <I, O> void reinterpret(VectorSpecies<I> src, VectorSpecies<O> dst, IntFunction<?> fa) {
         Object a = fa.apply(BUFFER_SIZE);
         reinterpret_kernel(src, dst, a);
+    }
+
+    @Test(dataProvider = "fixedShapeXSegmentedLegalCastSpecies")
+    static <E,F> void shuffleCast(VectorSpecies<E> src, VectorSpecies<F> dst) {
+        legal_shuffle_cast_kernel(src, dst);
+    }
+
+    @Test(dataProvider = "fixedShapeXSegmentedIllegalCastSpecies")
+    static <E,F> void shuffleCastNeg(VectorSpecies<E> src, VectorSpecies<F> dst) {
+        illegal_shuffle_cast_kernel(src, dst);
+    }
+
+    @Test(dataProvider = "fixedShapeXSegmentedLegalCastSpecies")
+    static <E,F> void maskCast(VectorSpecies<E> src, VectorSpecies<F> dst) {
+        legal_mask_cast_kernel(src, dst);
+    }
+
+    @Test(dataProvider = "fixedShapeXSegmentedIllegalCastSpecies")
+    static <E,F> void maskCastNeg(VectorSpecies<E> src, VectorSpecies<F> dst) {
+        illegal_mask_cast_kernel(src, dst);
     }
 }
