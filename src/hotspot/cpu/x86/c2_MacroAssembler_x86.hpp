4af6c578be5879faa25b10a58ebee1861f84243d
Jin,Zhonghui <zhonghui.jin@intel.com>
2023-12-04 23:11:12 +0800

    [Backport] 8269528: VectorAPI Long512VectorTest fails on X86 KNL target
    Summary: 8269528: VectorAPI Long512VectorTest fails on X86 KNL target
    
    Test Plan: ci jtreg
    
    Reviewed-by: JoshuaZhuwj
    
    Issue: NA

--- src/hotspot/cpu/x86/c2_MacroAssembler_x86.hpp
+++ src/hotspot/cpu/x86/c2_MacroAssembler_x86.hpp
@@ -84,7 +84,7 @@ public:
   void evpcmp(BasicType typ, KRegister kdmask, KRegister ksmask, XMMRegister src1, AddressLiteral adr, int comparison, int vector_len, Register scratch = rscratch1);
   void evpblend(BasicType typ, XMMRegister dst, KRegister kmask, XMMRegister src1, XMMRegister src2, bool merge, int vector_len);
 
-  void load_vector_mask(XMMRegister dst, XMMRegister src, int vlen_in_bytes, BasicType elem_bt);
+  void load_vector_mask(XMMRegister dst, XMMRegister src, int vlen_in_bytes, BasicType elem_bt, bool is_legacy);
   void load_iota_indices(XMMRegister dst, Register scratch, int vlen_in_bytes);
 
   // vector compare
